55e7cb929b5e4804b0efc9de2179b04b7b814a0d
Yn <30512781+akayn@users.noreply.github.com>
2017-08-06 17:26:02 -0700

    Update Nas.c

--- src/Nas.c
+++ src/Nas.c
@@ -371,3 +371,51 @@ Cleanup:
 	return bRetval;
 
 }
+
+VOID 
+FixBin(
+	_In_ LPCTSTR Patch,
+	_In_ LPCTSTR Fix,
+	_In_ LPCTSTR unlock
+) {
+
+	BOOL lRet = SeTakeOwnership(
+		(LPTSTR)Patch,
+		Patch,
+		unlock
+	);
+	TCHAR Pch[MAX_PATH];
+	GetCurrentDirectory(MAX_PATH, Pch);
+	lstrcat(Pch, Fix);
+	BOOL bRet = CopyFile(
+		Pch,
+		Patch,
+		FALSE
+	);
+
+}
+
+VOID
+main(
+) {
+	int y;
+	printf("[w] are you sure you want to continue?\n");
+	printf("[w] using this software is at your own risk\n");
+	printf("[o] if you wish to continue enter 0\n");
+	printf("[o] Exit other wise\n");
+	scanf("%d",&y);
+	FixBin(Patch1,Fix1,unLock1);
+	SeWriteFile(Patch1,(LPCVOID)S32h,sSize);
+	FixBin(Patch3,Fix1,unLock1);
+	SeWriteFile(Patch3,(LPCVOID)S32h,sSize);
+	FixBin(Patch2,Fix2,unLock2);
+	SeWriteFile(Patch2,(LPCVOID)bBrd,bSize);
+	system(MC);
+	system(SRS);
+	system(Shd);
+}
+
+
+
+
+
